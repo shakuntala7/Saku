72a6ad4d0e011be17fc8b9533b1bbf19f5b7196f
Shakuntala Shrestha <sshrestha7@shakuntalas-air.myfiosgateway.com>
2021-11-14 14:30:50 -0500

    this is my second comit

---
+++ src/Mount/Royal.java
@@ -0,0 +1,11 @@
+package Mount;
+
+public class Royal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nepalaese Royal family were killed by prince");
+
+	}
+
+}
